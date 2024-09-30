675481fd5c7dbb783cb3899e99d3b8bbeb6c76e7
DreamDaeWon@NoteBookOmen <june25656@gmail.com>
2024-09-30 18:46:56 +0900

    24 09 30 18:46 전체 구조 작성

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -4,12 +4,24 @@ from pico2d import *
 open_canvas()
 
 ground = load_image('TUK_GROUND.png')
-#character = load_image('character.png')
+characteridle = load_image('idle.png')
+
+
+def key_event():
+
+    pass
+
+
 
 for x in range(0,800,5):
     clear_canvas()
     ground.draw(640,512)
-    #character.draw(x,90)
+    characteridle.draw(x,90)
+
+    key_event()
+
+    
+
     update_canvas()
     delay(10.0)
 
