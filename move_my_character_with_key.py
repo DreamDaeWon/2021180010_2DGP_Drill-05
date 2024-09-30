1c8e9a3b9b494a03d3ee42b5eebb0d95be620e6a
DreamDaeWon@NoteBookOmen <june25656@gmail.com>
2024-09-30 18:57:01 +0900

    24 09 30 18:56 Idle 상태 이미지 좌표값 수치 저장

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -6,6 +6,11 @@ open_canvas()
 ground = load_image('TUK_GROUND.png')
 characteridle = load_image('idle.png')
 
+idlenmm_x = [3,39,73,111,149,193,240,286,332]
+idlenmm_y = [50,50,50,50,50,50,50,52,53]
+idlenmm_w = [29,31,36,36,35,38,41,42,41]
+idlenmm_h = [41,40,38,37,36,40,38,37,36]
+
 
 def key_event():
 
@@ -20,7 +25,7 @@ for x in range(0,800,5):
 
     key_event()
 
-    
+
 
     update_canvas()
     delay(10.0)
